81746d14eafed745681b9677fbffdb8cc97194a3
Jordan Balch <inkybalch@gmail.com>
2025-10-10 19:02:24 +0100

    updated the count and newer presatation

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #   Prints the contents of a txt file.
-from stats import get_num_words, get_chars_dict
+from stats import get_num_words, get_chars_dict, get_sorted_dict
 
 
 def main():
-    book_path = "github.com/GilgameshVII/books/frankenstein.txt"
+    book_path = "/home/jordan/workspace/github.com/GilgameshVII/bookbot/books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
+    #chars_dict = get_chars_dict(text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzig book found at {book_path}")
+    print("----------- Word Count -----------")
     print(f"Found {num_words} total words")
-    print(chars_dict)
+    print("--------- Character Count ---------")
+    get_sorted_dict(text)
+    
+    
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -12,3 +12,20 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+def get_sorted_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if not lowered.isalpha():
+            continue
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+
+    chars = dict(sorted(chars.items(), key=lambda item: item[1], reverse=True))
+
+    for k, v in chars.items():
+        print(f"{k}: {v}")
+    return chars
