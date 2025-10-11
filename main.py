f7c9b0f17906eca2cc328451a6e4aacdbfdf6093
GilgameshVII <inkybalch@gmail.com>
2025-10-11 10:27:46 +0100

    made final changes for a exit code and a discription on how to use

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 #   Prints the contents of a txt file.
+import sys
 from stats import get_num_words, get_chars_dict, get_sorted_dict
 
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def main():
-    book_path = "/home/jordan/workspace/github.com/GilgameshVII/bookbot/books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     #chars_dict = get_chars_dict(text)
